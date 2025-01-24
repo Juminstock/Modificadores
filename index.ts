89febde8f252b43418906e516ad4cf5fe1201a5c
Carlos Rodríguez <juminstock@gmail.com>
2025-01-23 22:27:32 -0500

    update: minimal change

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import { Bot } from "grammy";
 
 const bot = new Bot("7865859030:AAFqVPMR_JNdBYBYv0sFWuUJh_tadxBGLXg");
 
-// Reply to any message with "Hi there!".
 bot.on("message", (ctx) => ctx.reply("Hi there, welcome to Soneium Bridger!"));
 
 bot.start();
